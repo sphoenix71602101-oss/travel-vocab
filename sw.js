940ed7236ef067122ec8687e8f50babd48692117
sphoenix71602101-oss <sphoenix71602101@gmail.com>
2026-09-15 21:28:44 +0800

    默认顶图替换

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
 // Increment this version whenever a precached application-shell file changes.
 const CACHE_PREFIX = "yujian-world-shell-";
 const LEGACY_CACHE_PREFIX = "travel-vocab-shell-";
-const CACHE_NAME = `${CACHE_PREFIX}v23`;
+const CACHE_NAME = `${CACHE_PREFIX}v25`;
 const APP_SHELL = [
   "./",
   "index.html",
