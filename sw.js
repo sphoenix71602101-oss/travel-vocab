a3a56abd04e13fa377af7a4b69c26d445393e001
sphoenix71602101-oss <sphoenix71602101@gmail.com>
2026-09-22 16:09:17 +0800

    PT

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
 // Increment this version whenever a precached application-shell file changes.
 const CACHE_PREFIX = "yujian-world-shell-";
 const LEGACY_CACHE_PREFIX = "travel-vocab-shell-";
-const CACHE_NAME = `${CACHE_PREFIX}v54`;
+const CACHE_NAME = `${CACHE_PREFIX}v55`;
 const APP_SHELL = [
   "./",
   "index.html",
